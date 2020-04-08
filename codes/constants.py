8902eae66a38665451af7984b45af762f80fcbae
Anowarul Kabir <akabir0101@gmail.com>
2020-04-08 01:21:58 -0400

    new constants added

--- codes/constants.py
+++ codes/constants.py
@@ -4,11 +4,25 @@ PDB_DIR = "../pdbs/"
 DISTANCE_MATRIX_DIR = "../distance_matrices/"
 CONTACT_MAP_DIR = "../contact_maps/"
 FASTA_DIR = "../fastas/"
+FEATURE_DIR = "../features/"
 
 # file format
 CIF = 'mmCif'
+FASTA = 'fasta'
 
 # extensions
 CIF_EXT = ".cif"
 CSV_EXT = ".csv"
 FASTA_EXT = ".fasta"
+PT_EXT = ".pt"
+
+# amino acid letters from wiki
+AMINO_ACID = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+              'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+              'T', 'U', 'V', 'W', 'Y', 'X', 'Z']
+AMINO_ACID_20 = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
+                 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V',
+                 'W', 'Y']
+
+# filename padding
+_ONE_HOT = "_one_hot"
