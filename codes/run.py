5e622deaf02d273131f8b5fe80e03550a269b222
Anowarul Kabir <akabir0101@gmail.com>
2020-04-08 01:26:21 -0400

    edited gitignore, runner files

--- codes/run.py
+++ codes/run.py
@@ -1,21 +1,10 @@
 from Bio import SeqIO
 import os
 from pdb_converter import PDBConverter
+from feature_generator import FeatureGenerator
 
-pdb_converter = PDBConverter()
-pdb_converter.do()
+# pdb_converter = PDBConverter()
+# pdb_converter.do()
 
-
-# records = SeqIO.parse("../pdbs/2blq.cif", "cif-atom")
-# count = SeqIO.write(records, "../fastas/2blq.fasta", "fasta")
-# print("Converted %i records" % count)
-#
-#
-# records = SeqIO.parse("../fastas/2ok6.fasta", "fasta")
-# for record in records:
-#     print("Id: %s" % record.id)
-#     print("Name: %s" % record.name)
-#     print("Description: %s" % record.description)
-#     print("Annotations: %s" % record.annotations)
-#     print("Sequence Data: %s" % record.seq)
-#     print("Sequence Alphabet: %s" % record.seq.alphabet)
+feature_generator = FeatureGenerator()
+feature_generator.one_hot()
