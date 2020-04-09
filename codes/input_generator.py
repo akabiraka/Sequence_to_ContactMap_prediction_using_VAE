ead4c27399d1c667c541a207801807d39a259a0f
Anowarul Kabir <akabir0101@gmail.com>
2020-04-09 04:56:02 -0400

    CxHxW sequence output generated

--- codes/input_generator.py
+++ codes/input_generator.py
@@ -11,7 +11,7 @@ import constants as CONSTANTS
 class InputGenerator(object):
     """docstring for FeatureSelector."""
 
-    def __init__(self, window=30, stride=30):
+    def __init__(self, window=32, stride=32):
         super(InputGenerator, self).__init__()
         self.window = window
         self.stride = stride
@@ -19,17 +19,23 @@ class InputGenerator(object):
     def get_input_output(self, pdb_code):
         one_hot_tensor = Utility.read_one_hot_tensor(pdb_code)
         contact_map_tensor = Utility.read_contact_map_tensor(pdb_code)
-        print(pdb_code + ": ", one_hot_tensor.shape, contact_map_tensor.shape)
+        print(pdb_code + ":",
+              "1-hot size:", one_hot_tensor.shape,
+              "contact-map size:", contact_map_tensor.shape)
         rows, cols = one_hot_tensor.shape
         half_width = math.floor(self.window / 2)
         a_input_output_set = []
         all_input_output_set = []
         for i in range(half_width, rows - half_width, self.stride):
             sub_seq1 = one_hot_tensor[i - half_width:i + half_width]
+            sub_seq1 = sub_seq1.unsqueeze(0)
+            sub_seq1 = sub_seq1.type(torch.float32)
             for j in range(half_width, rows - half_width, self.stride):
                 # print(i - half_width, i + half_width,
                 #       j - half_width, j + half_width)
                 sub_seq2 = one_hot_tensor[j - half_width:j + half_width]
+                sub_seq2 = sub_seq2.unsqueeze(0)
+                sub_seq2 = sub_seq2.type(torch.float32)
                 out = contact_map_tensor[i - half_width:i +
                                          half_width, j - half_width:j + half_width]
                 a_input_output_set = [sub_seq1, sub_seq2, out]
