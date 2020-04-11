2962284075b10bc95ce3a498df3e228af60df297
Anowarul Kabir <akabir0101@gmail.com>
2020-04-11 02:16:27 -0400

    (input, output) pair wise data generation added, input is (seq1, seq2)

--- codes/input_generator.py
+++ codes/input_generator.py
@@ -11,12 +11,19 @@ import constants as CONSTANTS
 class InputGenerator(object):
     """docstring for FeatureSelector."""
 
-    def __init__(self, window=32, stride=32):
+    def __init__(self):
         super(InputGenerator, self).__init__()
-        self.window = window
-        self.stride = stride
+        self.window = CONSTANTS.WINDOW_SIZE
+        self.stride = CONSTANTS.WINDOW_STRIDE
 
-    def get_input_output(self, pdb_code):
+    def get_input_output_pre(self, pdb_code):
+        """
+        deprecated
+            output format:
+                [seq1, seq2, output_matrix]
+                size of seq1, seq2 = 1 x l x 20
+                output_matrix = l x l
+        """
         one_hot_tensor = Utility.read_one_hot_tensor(pdb_code)
         contact_map_tensor = Utility.read_contact_map_tensor(pdb_code)
         print(pdb_code + ":",
@@ -49,3 +56,50 @@ class InputGenerator(object):
         #     print(sub_seq1.shape, sub_seq2.shape, out.shape)
         # Utility.plot_images(all_outputs, img_name=pdb_code, cols=5)
         return all_input_output_set
+
+    def get_input_output(self, pdb_code):
+        """
+            output format:
+                input: concatenated sub_seq1, sub_seq2, [1 x 2n x self.window],
+                        n=20 for AA letters
+                out:
+
+                size of seq1, seq2 = 1 x l x 20
+                output_matrix = l x l
+        """
+        one_hot_tensor = Utility.read_one_hot_tensor(pdb_code)
+        contact_map_tensor = Utility.read_contact_map_tensor(pdb_code)
+        print(pdb_code + ":",
+              "1-hot size:", one_hot_tensor.shape,
+              "contact-map size:", contact_map_tensor.shape)
+        rows, cols = one_hot_tensor.shape
+        half_width = math.floor(self.window / 2)
+        a_input_output_set = []
+        all_input_output_set = []
+        for i in range(half_width, rows - half_width, self.stride):
+            sub_seq1 = self.format_seq(one_hot_tensor, i -
+                                       half_width, i + half_width)
+            for j in range(half_width, rows - half_width, self.stride):
+                # print(i - half_width, i + half_width,
+                #       j - half_width, j + half_width)
+                sub_seq2 = self.format_seq(one_hot_tensor, j -
+                                           half_width, j + half_width)
+                input = torch.cat((sub_seq1, sub_seq2), 1)
+                input.unsqueeze_(0)
+                # print(input.size())
+                out = contact_map_tensor[i - half_width:i +
+                                         half_width, j - half_width:j + half_width]
+
+                out = out.type(torch.float32)
+                a_input_output_set = [input, out]
+                all_input_output_set.append(a_input_output_set)
+            #     break
+            # break
+        return all_input_output_set
+
+    def format_seq(self, tensor, from_seq_index, to_seq_index):
+        sub_seq = tensor[from_seq_index:to_seq_index]
+        sub_seq.transpose_(0, 1)
+        sub_seq = sub_seq.type(torch.float32)
+        # print(sub_seq.size())
+        return sub_seq
--- codes/protein_dataset.py
+++ codes/protein_dataset.py
@@ -13,9 +13,10 @@ class ProteinDataset(Dataset):
 
     def __init__(self):
         super(ProteinDataset, self).__init__()
-        pdb_identifies_file = '../inputs/pdb_identifiers.txt'
+        pdb_identifies_file = '../inputs/train.txt'  # pdb_identifiers.txt'
         self.pdb_identifiers = Utility.get_pdb_identifiers(pdb_identifies_file)
         self.input_generator = InputGenerator()
+        self.records = self.generate_input_output_sets()
 
     def __len__(self):
         return len(self.pdb_identifiers)
@@ -24,3 +25,14 @@ class ProteinDataset(Dataset):
         pdb_code = self.pdb_identifiers[i]
         all_input_output_set = self.input_generator.get_input_output(pdb_code)
         return all_input_output_set
+
+    def generate_input_output_sets(self):
+        records = []
+        for identifier in self.pdb_identifiers:
+            pdb_code = identifier
+            inp_out_pairs = self.input_generator.get_input_output(pdb_code)
+            print(pdb_code, ":", len(inp_out_pairs))
+            records.extend(inp_out_pairs)
+
+        print(len(records))
+        return records
