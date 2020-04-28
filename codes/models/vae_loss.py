7ae6091b297d7129f7ef6477c7f2c6ff50ecde7a
Anowarul Kabir <akabir0101@gmail.com>
2020-04-28 16:44:04 -0400

    from vae loss BCE and KLD losses are differently used to see the output

--- codes/models/vae_loss.py
+++ codes/models/vae_loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import traceback
 
 import constants as CONSTANTS
 
@@ -14,16 +15,29 @@ class VAELoss(nn.Module):
 
     def forward(self, y, y_prime, mu, logvar):
         """
-            reconstruction + KL divergence loss
+            reconstruction + 3*KLD
         """
-        y_prime = y_prime.view(-1, self.w_size * self.w_size)
-        y = y.view(-1, self.w_size * self.w_size)
+        loss = 0.0
+        try:
+            y_prime = y_prime.view(-1, self.w_size * self.w_size)
+            y = y.view(-1, self.w_size * self.w_size)
 
-        BCE = F.binary_cross_entropy(y_prime, y, size_average=False)
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        # return BCE + KLD
-        return BCE + 3 * KLD
+            # BCE = F.binary_cross_entropy_with_logits(
+            #     y_prime, y, reduction='sum')
+            BCE = F.binary_cross_entropy(
+                y_prime, y, reduction='mean')
+            # print(BCE)
+            # see Appendix B from VAE paper:
+            # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+            # https://arxiv.org/abs/1312.6114
+            # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+            KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+            # print(KLD)
+            loss = BCE + 3 * KLD
+
+            # loss = BCE
+        except Exception as e:
+            print(y_prime.shape, y.shape)
+            traceback.print_exc()
+            raise
+        return loss
